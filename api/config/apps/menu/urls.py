cb2cf598ab1713828f9c67af589d6da11ea3122a
Adrian <aneuville99@gmail.com>
2026-02-03 21:08:41 +0100

    feat: implement Dish API with viewsets, serializers, and routing

--- api/config/apps/menu/urls.py
+++ api/config/apps/menu/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+from .views import DishViewSet
+
+router = DefaultRouter()
+router.register("", DishViewSet)
+
+urlpatterns = router.urls

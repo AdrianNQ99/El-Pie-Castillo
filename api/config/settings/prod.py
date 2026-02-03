98aee24090453bf8fc68137db7c7f85ed18591a5
Adrian <aneuville99@gmail.com>
2026-02-03 23:54:17 +0100

    Add start script for API with database migration and Gunicorn server

--- api/config/settings/prod.py
+++ api/config/settings/prod.py
@@ -3,7 +3,7 @@ import dj_database_url
 from .base import *
 
 DEBUG = False
-SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key-change-this')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = [
     os.environ.get('RENDER_EXTERNAL_URL', 'localhost'),
---
+++ api/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /app/api
+python manage.py migrate --noinput
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT

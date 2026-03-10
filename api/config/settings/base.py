0550ef28d1fe870d9e0c4df81e8cd7799244c56f
Adrian <aneuville99@gmail.com>
2026-03-10 19:08:02 +0100

    fix: update DEBUG setting to False for production and add .env to .gitignore

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0%o=*h9898pxpu6oj_j=%7exi&a69-zyg-6^lfb2dghk#y1&%u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
